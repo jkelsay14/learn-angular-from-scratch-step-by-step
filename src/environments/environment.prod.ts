56af92f57db7cdda32321a3b9dd52298d3c051c0
jkelsay14 <jaredkelsay@gmail.com>
2023-04-07 00:39:48 -0400

    add api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://ujm7dh2s09.execute-api.us-east-1.amazonaws.com'
 };
